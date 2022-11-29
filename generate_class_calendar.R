e5428737670d8924e498a3702a5bbc4e1951e1d7
Rolando Amir Mirabal Hinojosa <ramirabal@gmail.com>
2022-11-29 07:48:58 -0400

    calendar

--- generate_class_calendar.R
+++ generate_class_calendar.R
@@ -1,7 +1,7 @@
 install.packages("calendR")
 library(calendR)
 calendR(year = 2022,
-        special.days = c(332,335,336,340,342,346,348,350,354,356),
+        special.days = c(332,335,336,340,343,346,348,350,354,356),
         special.col = c("lightsalmon"))
     
         
