cc89ab8eaf089ffec784de7059d87d12b77a26d7
roammi <ramirabal@gmail.com>
2022-12-08 21:04:35 -0400

    updated github setup

--- github_setup.R
+++ github_setup.R
@@ -1,5 +1,9 @@
 # setgithub
 # happygitwithr
+
+#git config 
 install.packages('usethis')
+
+usethis::use_git_config(user.name = "roammi", user.email = "ramirabal@gmail.com")
 usethis::create_github_token()
 gitcreds::gitcreds_set()
